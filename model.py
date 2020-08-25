f75d6d6e7e8697dd366e2bed45f8c75c8eafd960
Tilen Klinc <tilen.cinc@gmail.com>
2020-08-25 17:40:51 +0200

    manjsi popravki, vecerja

--- model.py
+++ model.py
@@ -1,21 +1,19 @@
 import json
 
-# definirane konstante za prijavo
-UPORABNISKO_IME_NE_OBSTAJA = 'Vpisano uporabniško ime ne obstaja. Prosim, poskusite ponovno'
-UPORABNISKO_IME_ZE_OBSTAJA = 'To uporabniško ime je že v uporabi. Poskusite z drugim.'
-NAPACNO_GESLO = 'Vpisano geslo ni pravilno. Prosim, poskusite ponovno'
+# Definirane konstante za prijavo
+UPORABNIK_NE_OBSTAJA = 'Vpisano uporabniško ime ne obstaja. Prosim, poskusite ponovno.'
+UPORABNIK_ZASEDEN = 'To uporabniško ime je že v uporabi. Prosim, poskusite ponovno.'
+NAPACNO_GESLO = 'Vpisano geslo ni pravilno. Prosim, poskusite ponovno.'
 
-# ostale konstante
+# Ostale konstante
 VECKRATNA_POJAVITEV = 'V'
 KONTAKTA_NI = 'N'
-PRAZNO = 'Polje ne sme biti prazno'
+PRAZNO = 'Polje ne sme biti prazno.'
 
-# definiram razred "prijava", ki shranjuje uporabniška imena, gesla ter uporabniške povezave
-class Stiki:
-    def __init__(self, ime, priimek, stevilka, mail, naslov, krog):
-        self.ime = ime
-        self.priimek = priimek
-        self.stevilka = stevilka
-        self.mail = mail
-        self.naslov = naslov
-        self.krog = krog
+# Definiram razred "Prijava", ki shranjuje uporabniška imena in gesla
+# Povezave dopolnim z razredom "Povezave()"
+class Prijava:
+    def __init__(self, username, password, povezave):
+        self.username = username
+        self.password = password
+        self.povezave = povezave
