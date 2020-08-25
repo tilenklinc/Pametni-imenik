a7f1f90c86356396d2d2dd640dce399843c3d3bb
Tilen Klinc <tilen.cinc@gmail.com>
2020-08-25 17:58:34 +0200

    Dodana posodobitev stanja

--- model.py
+++ model.py
@@ -1,19 +1,34 @@
 import json
 
 # Definirane konstante za prijavo
-UPORABNIK_NE_OBSTAJA = 'Vpisano uporabniško ime ne obstaja. Prosim, poskusite ponovno.'
-UPORABNIK_ZASEDEN = 'To uporabniško ime je že v uporabi. Prosim, poskusite ponovno.'
-NAPACNO_GESLO = 'Vpisano geslo ni pravilno. Prosim, poskusite ponovno.'
+WRONG_USERNAME = "Vpisano uporabniško ime ne obstaja. Prosim, poskusite ponovno."
+USER_TAKEN = "To uporabniško ime je že v uporabi. Prosim, poskusite ponovno."
+WRONG_PASS = "Vpisano geslo ni pravilno. Prosim, poskusite ponovno."
 
 # Ostale konstante
-VECKRATNA_POJAVITEV = 'V'
-KONTAKTA_NI = 'N'
-PRAZNO = 'Polje ne sme biti prazno.'
+MULTIPLE = "V"
+NO_CONTACT = "N"
+EMPTY = "Polje ne sme biti prazno."
 
 # Definiram razred "Prijava", ki shranjuje uporabniška imena in gesla
 # Povezave dopolnim z razredom "Povezave()"
+# Preverim ustreznost gesla, si zapomnim stanje
 class Prijava:
+
     def __init__(self, username, password, povezave):
         self.username = username
         self.password = password
         self.povezave = povezave
+
+    def preveri_password(self, password):
+        if self.password != password:
+            return WRONG_PASS
+
+    def update_file(self, in_file):
+        slovar = {
+            "username": self.username,
+            "password": self.password,
+            "update": self.povezave.povezave
+        }
+        with open(in_file, "w", encoding="utf-8") as vhodna: #json
+            json.dump(slovar_stanja, in_file, ensure_ascii=False, indent=4)
